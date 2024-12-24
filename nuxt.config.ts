c05dccf534f607426228d620c624feb940b10362
j.mclain <joshmclain45@gmail.com>
2024-12-23 20:42:55 -0700

    :wrench: (style): add tailwind config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,29 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ["@nuxt/ui", "@nuxtjs/supabase", "@nuxt/eslint", '@pinia/nuxt', 'pinia-plugin-persistedstate/nuxt'],
+  modules: [
+    "@nuxt/ui",
+    "@nuxtjs/supabase",
+    "@nuxt/eslint",
+    "@pinia/nuxt",
+    "pinia-plugin-persistedstate/nuxt",
+  ],
   supabase: {
     url: process.env.SUPABASE_URL,
     key: process.env.SUPABASE_ANON_KEY,
-  }
-})
+  },
+  tailwindcss: {
+    cssPath: "~/assets/css/tailwind.css",
+    config: {
+      theme: {
+        extend: {
+          aspectRatio: {
+            auto: "auto",
+            square: "1 / 1",
+            video: "16 / 9",
+          },
+        },
+      },
+    },
+  },
+});
